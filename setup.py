e1943d0eb3a5434fc4a9f8c3ba04f183d7365df2
matt <matt@quantasylum.com>
2024-07-22 09:23:42 -0600

    Fixed version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyQa40x',
-    version='0.2.0',
+    version='0.2.1',
     description='A Python library for the QA40x audio analyzer',
     author='Your Name',
     author_email='your.email@example.com',
